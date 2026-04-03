0bfec821791f33d958a99a148ddf70fab854c0be
thisiskarthick-5 <karthickfaf19.7@gmail.com>
2026-04-03 22:17:46 +0530

    updated app.py

--- api.py
+++ api.py
@@ -1,22 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
-import numpy as np
 
 app = Flask(__name__)
-CORS(app)   # 🔥 IMPORTANT LINE
+CORS(app)
 
+# Load model
 model = pickle.load(open("model.pkl", "rb"))
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    hours = float(data['hours'])   # convert to float
+    hours = float(data['hours'])
 
     prediction = model.predict([[hours]])
 
     return jsonify({
-        'predicted_marks': float(prediction[0])
+        "predicted_marks": float(prediction[0])
     })
 
 if __name__ == '__main__':
